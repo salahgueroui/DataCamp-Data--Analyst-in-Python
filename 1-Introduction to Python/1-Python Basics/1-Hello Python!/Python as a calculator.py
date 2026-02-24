80c62b5c31b9e457955a6b124cd2875ef64ccb7f
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-24 23:44:15 +0100

    Add py solution

---
+++ 1-Introduction to Python/1-Python Basics/1-Hello Python!/Python as a calculator.py
@@ -0,0 +1,18 @@
+## Exercise
+## Python as a calculator
+
+## Instructions
+## Print the result of subtracting 5 from 5 under # Subtraction using print().
+## Print the result of multiplying 3 by 5 under # Multiplication.
+
+## MY SOLUTION:
+
+# Addition and division
+print(4 + 5)
+print(10 / 2)
+
+# Subtraction
+print(5-5)
+
+# Multiplication
+print(3*5)
