2be6e5b81ea819dab07cb009821a93eb41744eb6
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-25 00:27:28 +0100

    Add py solution

---
+++ 1-Introduction to Python/2-Python Lists/1-Python Lists/3-List of lists.py
@@ -0,0 +1,27 @@
+## Exercise
+## List of lists
+
+## Instructions
+## Finish the list of lists so that it also contains
+## the bedroom and bathroom data.
+## Make sure you enter these in order.
+## Print out house.
+
+## MY SOLUTION:
+
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# House information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ['bedroom', bed],
+         ['bathroom', bath]
+         ]
+
+# Print out house
+print(house)
