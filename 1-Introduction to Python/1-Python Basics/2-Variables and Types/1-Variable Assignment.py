26e85200c272f6d24af1f9dc9263a8acab13609b
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-24 23:56:17 +0100

    Add py solution

---
+++ 1-Introduction to Python/1-Python Basics/2-Variables and Types/1-Variable Assignment.py
@@ -0,0 +1,8 @@
+## Exercise
+## Variable Assignment
+
+## Instructions
+## Create a variable savings with the value of 100.
+## Check out this variable by typing print(savings) in the script.
+
+## MY SOLUTION:
