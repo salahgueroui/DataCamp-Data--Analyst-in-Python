65c342459730765ab7c45df0482dcb9c34a92ea8
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-25 00:08:17 +0100

    Add py solution

---
+++ 1-Introduction to Python/1-Python Basics/2-Variables and Types/4-Operations with other types.py
@@ -0,0 +1,28 @@
+## Exercise
+## Operations with other types
+
+## Instructions 1/2
+## Add savings and new_savings and assign it to total_savings.
+## Use type() to print the resulting type of total_savings.
+
+## MY SOLUTION:
+
+savings = 100
+new_savings = 40
+# Calculate total_savings using savings and new_savings
+total_savings=savings+new_savings
+print(total_savings)
+# Print the type of total_savings
+print(int)
+
+## Instructions 2/2
+## Calculate the sum of intro and intro and assign the result to doubleintro.
+## Print out doubleintro.
+
+## MY SOLUTION:
+
+intro = "Hello! How are you?"
+# Assign sum of intro and intro to doubleintro
+doubleintro=intro+intro
+# Print out doubleintro
+print(doubleintro)
