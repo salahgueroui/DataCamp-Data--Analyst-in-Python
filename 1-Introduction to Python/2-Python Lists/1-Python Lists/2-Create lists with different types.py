70387404cce91b8c65d3d5056d089d45e16a704e
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-25 00:19:09 +0100

    Add py solution

---
+++ 1-Introduction to Python/2-Python Lists/1-Python Lists/2-Create lists with different types.py
@@ -0,0 +1,22 @@
+## Exercise
+## Create lists with different types
+
+## Instructions
+## Finish the code that creates the areas list.
+#  Build the list so that it first contains the name of each room as a string and then its area.
+#  Add the strings "hallway", "kitchen" and "bedroom" at the appropriate locations.
+## Print areas again.
+
+## MY SOLUTION:
+
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Adapt list areas
+areas = ['hallway', hall, 'kitchen', kit, "living room", liv, 'bedroom', bed, "bathroom", bath]
+
+# Print areas
+print(areas)
