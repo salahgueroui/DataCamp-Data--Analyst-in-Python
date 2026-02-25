a811ab606cd81495f0aa443f7558333cd92b2aae
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-25 01:35:47 +0100

    Add py solution

---
+++ 1-Introduction to Python/3-Functions and Packages/1-Functions/2-Help!.py
@@ -0,0 +1,11 @@
+## Exercise
+## Help!
+
+## Instructions
+## Use the IPython Shell to open up the documentation on pow()
+## by typing ?pow or help(pow).
+## Determine which statement about pow() is true.
+
+## MY SOLUTION:
+
+##pow() requires base and exp arguments; mod is optional.
