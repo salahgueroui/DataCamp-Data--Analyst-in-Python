b8d292251c6eed277b59f982f3085965a86c8ee2
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-25 18:08:29 +0100

    Add py solution

---
+++ 2-Intermediate Python/1-Matplotlib/1-Basic plots with Matplotlib/5-Scatter plot (2).py
@@ -0,0 +1,22 @@
+## Exercise
+## Scatter plot (2)
+
+## Instructions
+## - Import matplotlib.pyplot as plt.
+## - Build a scatter plot with:
+##     pop on the horizontal axis
+##     life_exp on the vertical axis
+## - Finish with plt.show() to display the plot.
+
+## MY SOLUTION:
+
+'''
+# Import package
+import matplotlib.pyplot as plt
+
+# Build Scatter plot
+plt.scatter(pop,life_exp,)
+
+# Show plot
+plt.show()
+'''
