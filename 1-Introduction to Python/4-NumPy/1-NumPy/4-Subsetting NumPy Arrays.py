7393f3e1bc1df5e3e06c1eb8d2be4db928e2ad56
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-25 04:10:08 +0100

    Add py solution

--- 1-Introduction to Python/4-NumPy/1-NumPy/4-Subsetting NumPy Arrays.py
+++ 1-Introduction to Python/4-NumPy/1-NumPy/4-Subsetting NumPy Arrays.py
@@ -10,10 +10,10 @@
 
 import numpy as np
 
-np_weight_lb = np.array(weight_lb)
-np_height_in = np.array(height_in)
+##np_weight_lb = np.array(weight_lb)
+##np_height_in = np.array(height_in)
 
 # Print out the weight at index 50
-print(np_weight_lb[50])
+#print(np_weight_lb[50])
 # Print out sub-array of np_height_in: index 100 up to and including index 110
-print(np_height_in[100:110])
+#print(np_height_in[100:110])
