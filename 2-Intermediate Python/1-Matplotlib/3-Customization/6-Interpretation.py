043016e61df3caed6c454a1c6b7efb55c7b92abe
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-26 00:49:00 +0100

    Add py solution

---
+++ 2-Intermediate Python/1-Matplotlib/3-Customization/6-Interpretation.py
@@ -0,0 +1,11 @@
+## Exercise
+## Interpretation
+
+## Question:
+## What can you say about the plot?
+
+## MY ANSWER:
+
+'''
+The countries in blue, corresponding to Africa, have both low life expectancy and a low GDP per capita.
+'''
