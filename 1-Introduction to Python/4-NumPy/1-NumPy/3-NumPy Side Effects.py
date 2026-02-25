703e7a3bc2261d7d33c12c3af9c3c338d93270a5
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-25 04:02:08 +0100

    Add py solution

--- 1-Introduction to Python/4-NumPy/1-NumPy/3-NumPy Side Effects.py
+++ 1-Introduction to Python/4-NumPy/1-NumPy/3-NumPy Side Effects.py
@@ -1,2 +1,10 @@
-import numpy as np
-np.array([True, 1, 2]) + np.array([3, 4, False])
+## Exercise
+## NumPy Side Effects
+
+## Instructions
+## Select the code that results in:
+## np.array([True, 1, 2]) + np.array([3, 4, False])
+
+## Correct answer:
+
+##np.array([4, 3, 0]) + np.array([0, 2, 2])
