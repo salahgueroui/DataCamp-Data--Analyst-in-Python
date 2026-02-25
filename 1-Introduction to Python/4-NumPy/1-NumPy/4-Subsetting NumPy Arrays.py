f11239347833e3755289182ae11a9152b9062ed4
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-25 04:02:19 +0100

    Add py solution

---
+++ 1-Introduction to Python/4-NumPy/1-NumPy/4-Subsetting NumPy Arrays.py
@@ -0,0 +1,19 @@
+## Exercise
+## Subsetting NumPy Arrays
+
+## Instructions
+## Print the element at index 50 of np_weight_lb.
+## Print a sub-array of np_height_in from index 100
+## up to and including index 110.
+
+## MY SOLUTION:
+
+import numpy as np
+
+np_weight_lb = np.array(weight_lb)
+np_height_in = np.array(height_in)
+
+# Print out the weight at index 50
+print(np_weight_lb[50])
+# Print out sub-array of np_height_in: index 100 up to and including index 110
+print(np_height_in[100:110])
