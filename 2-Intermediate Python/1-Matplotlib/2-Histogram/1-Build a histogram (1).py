30ac40c2090369a2c3d8c1771c8454d691228c37
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-25 18:23:57 +0100

    Add py solution

---
+++ 2-Intermediate Python/1-Matplotlib/2-Histogram/1-Build a histogram (1).py
@@ -0,0 +1,17 @@
+## Exercise
+## Build a histogram (1)
+
+## Instructions
+## - Use plt.hist() to create a histogram of life_exp.
+## - Do not specify the number of bins.
+## - Add plt.show() to display the histogram.
+
+## MY SOLUTION:
+
+'''
+# Create histogram of life_exp data
+plt.hist(life_exp)
+
+# Display histogram
+plt.show()
+'''
