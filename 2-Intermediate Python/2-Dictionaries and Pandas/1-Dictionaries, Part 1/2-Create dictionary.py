0c81a6c03c5a794dda951d334911cb8a503f6b9f
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-26 01:16:21 +0100

    Add py solution

---
+++ 2-Intermediate Python/2-Dictionaries and Pandas/1-Dictionaries, Part 1/2-Create dictionary.py
@@ -0,0 +1,19 @@
+## Exercise
+## Create dictionary
+
+## Instructions
+## - Create a dictionary called europe using countries as keys and capitals as values.
+## - Make sure everything is lowercase.
+## - Print europe.
+
+## MY SOLUTION:
+
+# Definition of countries and capital
+countries = ['spain', 'france', 'germany', 'norway']
+capitals = ['madrid', 'paris', 'berlin', 'oslo']
+
+# From string in countries and capitals, create dictionary europe
+europe = {'spain':'madrid','france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print europe
+print(europe)
---
+++ 2-Intermediate Python/2-Dictionaries and Pandas/1-Dictionaries, Part 1/3-Access dictionary.py
@@ -0,0 +1,17 @@
+## Exercise
+## Access dictionary
+
+## Instructions
+## - Print the keys of europe.
+## - Print the value that belongs to the key 'norway'.
+
+## MY SOLUTION:
+
+# Definition of dictionary
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# Print out the keys in europe
+print(europe.keys())
+
+# Print out value that belongs to key 'norway'
+print(europe['norway'])
