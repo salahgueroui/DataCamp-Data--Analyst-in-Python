a0843978ec31aa613f4eadc062f1b8c7a6e23e2f
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-27 16:42:40 +0100

    Add py solution

---
+++ 2-Intermediate Python/3-Logic Control Flow and Filtering/3-if, elif, else/2-if.py
@@ -0,0 +1,19 @@
+## Exercise
+
+## Instructions
+## - Examine the if statement for room == "kit".
+## - Write another if statement that prints "big place!" if area > 15.
+
+## MY SOLUTION:
+
+# Define variables
+room = "kit"
+area = 14.0
+
+# if statement for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+
+# if statement for area
+if area > 15 :
+    print("big place!")
