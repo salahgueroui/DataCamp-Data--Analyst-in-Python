ba9733244324d21cdcd7924e1835c4e18c300b8c
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-28 00:43:28 +0100

    Add py solution

---
+++ 2-Intermediate Python/3-Logic Control Flow and Filtering/3-if, elif, else/1-Warmup.py
@@ -0,0 +1,16 @@
+## Exercise
+## Warmup
+##area = 10.0
+##  if(area < 9) :
+##      print("small")
+##  elif(area < 12) :
+##      print("medium")
+##  else :
+##      print("large")
+
+## Instructions
+## - Determine the output of the given if/elif/else code block.
+## MY SOLUTION:
+'''
+medium
+'''
