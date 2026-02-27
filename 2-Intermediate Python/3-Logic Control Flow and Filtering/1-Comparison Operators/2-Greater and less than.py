bc2c7acb6a546e3f83879bbb777b54260cdd56f3
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-27 14:51:49 +0100

    Add py solution

---
+++ 2-Intermediate Python/3-Logic Control Flow and Filtering/1-Comparison Operators/2-Greater and less than.py
@@ -0,0 +1,21 @@
+## Exercise
+## Greater and less than
+
+## Instructions
+## - Write Python expressions, wrapped in a print() function, to check whether:
+##   * x is greater than or equal to -10.
+##   * "test" is less than or equal to y.
+##   * True is greater than False.
+
+## MY SOLUTION:
+
+# Comparison of integers
+x = -3 * 6
+print(x>=-10)
+
+# Comparison of strings
+y = "test"
+print("test"<=y)
+
+# Comparison of booleans
+print(True > False)
