abaaa0f9fd53fffbbeba8eed75e6e9384abd296e
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-28 14:16:50 +0100

    Add py solution

--- 2-Intermediate Python/4-Loops/4-Loop Data Structures Part 2/3-Add column (1).py
+++ 2-Intermediate Python/4-Loops/4-Loop Data Structures Part 2/3-Add column (1).py
@@ -16,6 +16,7 @@
 
 ## MY SOLUTION:
 
+'''
 # Import cars data
 import pandas as pd
 cars = pd.read_csv('cars.csv', index_col = 0)
@@ -25,3 +26,4 @@ for lab,row in cars.iterrows():
     cars.loc[lab,'COUNTRY']= (row['country']).upper()
 # Print cars
 print(cars)
+'''
