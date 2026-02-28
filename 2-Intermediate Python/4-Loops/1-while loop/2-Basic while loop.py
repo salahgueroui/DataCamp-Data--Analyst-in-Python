8fd1235bcd2c2754f05ff6309d35afb8c920c261
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-28 02:08:44 +0100

    Add py solution

---
+++ 2-Intermediate Python/4-Loops/1-while loop/2-Basic while loop.py
@@ -0,0 +1,20 @@
+## Exercise
+## Basic while loop
+
+## Instructions
+## - Create the variable offset with an initial value of 8.
+## - Code a while loop that runs as long as offset is not equal to 0.
+## - Inside the while loop:
+##   - Print the sentence "correcting...".
+##   - Decrease offset by 1.
+##   - Print offset to see how it changes.
+
+## MY SOLUTION:
+
+# Initialize offset
+offset=8
+
+# Code the while loop
+while offset!=0 :
+    print ("correcting...")
+    offset=offset-1
