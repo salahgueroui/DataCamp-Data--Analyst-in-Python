161f62c22e680f8522a1fa5138deee2b58238763
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-28 02:11:11 +0100

    Add py solution

---
+++ 2-Intermediate Python/4-Loops/2-for loop/1-Loop over a list.py
@@ -0,0 +1,18 @@
+## Exercise
+## Loop over a list
+
+## Instructions
+## - Write a for loop
+##   that iterates over all elements
+##   of the areas list.
+## - Print out every element
+##   separately.
+
+## MY SOLUTION:
+
+# areas list
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Code the for loop
+for rooms in areas :
+    print(rooms)
