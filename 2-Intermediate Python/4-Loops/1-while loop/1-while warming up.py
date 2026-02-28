9ab5d78978085380423668634e268ecee264d75d
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-28 02:07:02 +0100

    Add py solution

---
+++ 2-Intermediate Python/4-Loops/1-while loop/1-while warming up.py
@@ -0,0 +1,11 @@
+## Exercise
+## while: warming up
+
+## Instructions
+## - Examine the while loop provided.
+## - Determine how many times print(x) is executed.
+## - Choose the correct number of printouts.
+
+## MY SOLUTION:
+
+3
