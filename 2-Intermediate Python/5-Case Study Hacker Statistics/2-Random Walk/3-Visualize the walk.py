16dd86678f91a163c06a6480576e2296f3a47a24
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-28 18:03:15 +0100

    Add py solution

---
+++ 2-Intermediate Python/5-Case Study Hacker Statistics/2-Random Walk/3-Visualize the walk.py
@@ -0,0 +1,37 @@
+## Exercise
+## Visualize the walk
+
+## Instructions
+## - After the for loop, import matplotlib.pyplot as plt.
+## - Use plt.plot() to plot random_walk.
+## - Finish with plt.show() to display the plot.
+
+## MY SOLUTION:
+'''
+# NumPy is imported, seed is set
+
+# Initialization
+random_walk = [0]
+
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
+
+    if dice <= 2:
+        step = max(0, step - 1)
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    random_walk.append(step)
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Plot random_walk
+plt.plot(random_walk)
+
+# Show the plot
+plt.show()
+'''
