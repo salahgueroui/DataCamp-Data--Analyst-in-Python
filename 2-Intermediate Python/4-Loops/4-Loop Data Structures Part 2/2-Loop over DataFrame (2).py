b6355db98524cbc91dd1691a8fe9e9cf416fe286
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-28 02:31:49 +0100

    Add py solution

---
+++ 2-Intermediate Python/4-Loops/4-Loop Data Structures Part 2/2-Loop over DataFrame (2).py
@@ -0,0 +1,26 @@
+## Exercise
+## Loop over DataFrame (2)
+
+## Instructions
+## - Use the iterators lab and row.
+## - Adapt the for loop.
+## - On the first iteration,
+##   it should print:
+##   "US: 809"
+## - On the second iteration,
+##   it should print:
+##   "AUS: 731"
+## - Output format must be:
+##   "country: cars_per_cap"
+## - Make sure spacing is exact.
+## - Use str() if needed.
+
+## MY SOLUTION:
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Adapt for loop
+for lab, row in cars.iterrows() :
+    print(lab+': '+ str (  row [ 'cars_per_cap']))
