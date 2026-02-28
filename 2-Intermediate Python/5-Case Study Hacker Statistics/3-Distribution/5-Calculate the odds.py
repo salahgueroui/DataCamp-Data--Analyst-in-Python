93aa5ba0da62a61c6739d00ec0ea6059cbc7c69d
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-28 18:09:43 +0100

    Add py solution

---
+++ 2-Intermediate Python/5-Case Study Hacker Statistics/3-Distribution/5-Calculate the odds.py
@@ -0,0 +1,13 @@
+## Exercise
+## Calculate the odds
+
+## Instructions
+## - From the ends array, calculate how many values are >= 60.
+## - Divide this count by 500 (the total number of simulations).
+## - Determine the estimated probability of reaching at least 60 steps.
+
+## MY SOLUTION:
+
+'''
+78.4%
+'''
