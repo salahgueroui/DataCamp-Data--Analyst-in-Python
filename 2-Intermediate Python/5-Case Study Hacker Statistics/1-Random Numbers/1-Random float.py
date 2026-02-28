e4310843410f3deb281fc185e46acfd8500b6fac
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-28 17:53:44 +0100

    Add py solution

---
+++ 2-Intermediate Python/5-Case Study Hacker Statistics/1-Random Numbers/1-Random float.py
@@ -0,0 +1,22 @@
+## Exercise
+## Random float
+
+## Instructions
+## - Import numpy as np.
+## - Use seed()
+##   and pass 123 as the argument.
+## - Generate your first random float
+##   using rand().
+## - Print the result.
+
+## MY SOLUTION:
+'''
+# Import numpy as np
+import numpy as np
+
+# Set the seed
+np.random.seed(123)
+
+# Generate and print random float
+print(np.random.rand())
+'''
