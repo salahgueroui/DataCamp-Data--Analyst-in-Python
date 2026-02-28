44d9e43e57de2f375394804df6db185dba54ff92
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-28 18:02:01 +0100

    Add py solution

---
+++ 2-Intermediate Python/5-Case Study Hacker Statistics/2-Random Walk/2-How low can you go.py
@@ -0,0 +1,31 @@
+## Exercise
+## How low can you go?
+
+## Instructions
+## - Modify the code so that step never goes below 0.
+## - Use max() when dice <= 2 to prevent negative steps.
+## - Submit and check the contents of random_walk.
+
+## MY SOLUTION:
+'''
+# NumPy is imported, seed is set
+
+# Initialize random_walk
+random_walk = [0]
+
+for x in range(100) :
+    step = random_walk[-1]
+    dice = np.random.randint(1,7)
+
+    if dice <= 2:
+        # Replace below: use max to make sure step can't go below 0
+        step =max(0,step-1)  
+    elif dice <= 5:
+        step = step + 1
+    else:
+        step = step + np.random.randint(1,7)
+
+    random_walk.append(step)
+
+print(random_walk)
+'''
