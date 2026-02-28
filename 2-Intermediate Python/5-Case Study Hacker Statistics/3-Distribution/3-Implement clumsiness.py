5232bdffe45e3b37ed6be4ff52b8add57d7d7c41
Salah Gueroui <salaheddinegueroui50@gmail.com>
2026-02-28 18:07:12 +0100

    Add py solution

---
+++ 2-Intermediate Python/5-Case Study Hacker Statistics/3-Distribution/3-Implement clumsiness.py
@@ -0,0 +1,41 @@
+## Exercise
+## Implement clumsiness
+
+## Instructions
+## - Change the top-level range() so the simulation runs 20 times.
+## - Inside the walk loop, generate a random float with np.random.rand().
+## - If this random float is <= 0.005, reset step to 0.
+
+## MY SOLUTION:
+'''
+# numpy and matplotlib imported, seed set
+
+# clear the plot so it doesn't get cluttered if you run this many times
+plt.clf()
+
+# Simulate random walk 20 times
+all_walks = []
+for i in range(20) :
+    random_walk = [0]
+    for x in range(100) :
+        step = random_walk[-1]
+        dice = np.random.randint(1,7)
+        if dice <= 2:
+            step = max(0, step - 1)
+        elif dice <= 5:
+            step = step + 1
+        else:
+            step = step + np.random.randint(1,7)
+
+        # Implement clumsiness
+        if np.random.rand()<=0.005 :
+            step = 0
+
+        random_walk.append(step)
+    all_walks.append(random_walk)
+
+# Create and plot np_aw_t
+np_aw_t = np.transpose(np.array(all_walks))
+plt.plot(np_aw_t)
+plt.show()
+'''
